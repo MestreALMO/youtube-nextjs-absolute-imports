924e55b49f860ce568e11067daf453848326142b
André Lusegardis <lusegardis@gmail.com>
2023-03-11 10:52:34 -0300

    project done

---
+++ src/components/box1/box2/box3/box4/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/button";
+
+export const Box4 = () => {
+  return (
+    <>
+      <div style={{ border: "1px solid red" }}>
+        <h2>Box 4</h2>
+        <p>some change</p>
+        <Button />
+      </div>
+    </>
+  );
+};
